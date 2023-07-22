8bb807e6d79872779448e8a477874fdaa0c85950
Mr-RealMan <49441817+Mr-RealMan@users.noreply.github.com>
2023-07-22 15:22:31 +0800

    Add files via upload

---
+++ YTimeCtrl.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+
+/**************************************************
+* 类 名 称 ：
+* 类 描 述 ：
+* 作    者 ：
+* 创建时间 ：
+* 更新时间 ：
+* 版 本 号 ：v1.0.0
+**************************************************/
+
+public class YTimeCtrl : MonoBehaviour
+{
+    private int year;
+    private int month;
+    private int day;
+
+    IEnumerator Start()
+    {
+        yield return new WaitForSeconds(1);
+        year = DateTime.Now.Year;
+        month = DateTime.Now.Month;
+        day = DateTime.Now.Day;
+
+        DateTime dateLast = new DateTime(2022, 12, 9);
+        int c = DateTime.Compare(DateTime.Now, dateLast);
+        if(c>0)
+        {
+            Debug.Log("quit");
+            Application.Quit();
+        }
+    }
+}
